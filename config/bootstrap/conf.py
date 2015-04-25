1007488c255e77c1763a96dd05a650f78370aca6
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-04-25 17:08:38 -0500

    update copyright notice
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- config/bootstrap/conf.py
+++ config/bootstrap/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Research Handbook'
-copyright = u'2014, CS Department'
+copyright = u'2004-Present, Loyola University Chicago, Department of Computer Science'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
