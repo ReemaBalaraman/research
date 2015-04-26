2d4e949fc3bb14f0489a7bc839ab8d99bff7cd9a
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-04-25 23:00:13 -0500

    various fixes to deployment scripts
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- xbuild.sh
+++ xbuild.sh
@@ -1,6 +1,6 @@
 [ -f ~/.env/sphinx/bin/activate ] && . ~/.env/sphinx/bin/activate
 
 make clean
-make html
+make CONFIG=bootstrap html
 make latexpdf
 make epub
--- xgit-deploy.sh
+++ xgit-deploy.sh
@@ -3,4 +3,4 @@
 (git pull | head -1 | grep -v ^Already) \
 	&& echo "git log for $(date)" \
 	&& git --no-pager log -5 --pretty=oneline \
-	&& ./deploy-researchhandbook.sh
+	&& ./xdeploy.sh
